3265ae481c6f7df469a1b45584fc0d715cef0e7c
dc3671 <chenzhh3671@gmail.com>
2016-10-01 01:20:25 +0800

    feat(install): fix the install script

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
-#!/usr/bin/sh
+#!/bin/bash
 
 # vim
-curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
+mv ~/.vimrc.local ~/.vimrc.local.bak > /dev/null 2>&1
 ln -s $PWD/.vimrc.local ~/.vimrc.local
 ln -s $PWD/.vimrc.before.local ~/.vimrc.before.local
 ln -s $PWD/.vimrc.bundles.local ~/.vimrc.bundles.local
 
 # tmux
-mv ~/.tmux.conf ~/.tmux.conf.bak
+mv ~/.tmux.conf ~/.tmux.conf.bak >/dev/null 2>&1
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 
 # zsh
-mv ~/.zshrc ~/.zshrc.bak
+mv ~/.zshrc ~/.zshrc.bak >/dev/null 2>&1
 ln -s $PWD/.zshrc ~/.zshrc
 
 # eslint
-mv ~/.eslintrc.json ~/.eslintrc.json.bak
+mv ~/.eslintrc.json ~/.eslintrc.json.bak >/dev/null 2>&1
 ln -s $PWD/.eslintrc.json ~/.eslintrc.json
 
 # ternjs
-mv ~/.tern-config ~/.tern-config.bak
+mv ~/.tern-config ~/.tern-config.bak >/dev/null 2>&1
 ln -s $PWD/.tern-config ~/.tern-config
