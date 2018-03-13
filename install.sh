93fd507b723c513f07dcfd3762055f403b32c158
dc3671 <chenzhh3671@gmail.com>
2018-03-13 18:08:06 +0800

    feat(install): add auto switch shell command

--- install.sh
+++ install.sh
@@ -83,5 +83,18 @@ mv ~/.tmux.conf ~/.tmux.conf.bak >/dev/null 2>&1
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 success     "Done."
 
+TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+  # If this platform provides a "chsh" command (not Cygwin), do it, man!
+  if hash chsh >/dev/null 2>&1; then
+    printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+    chsh -s $(grep /zsh$ /etc/shells | tail -1)
+  # Else, suggest the user do so manually.
+  else
+    printf "I can't change your shell automatically because this system does not have chsh.\n"
+    printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
+  fi
+fi
+
 msg         "\nThanks for using my dotfiles."
 msg         "© `date +%Y` https://github.com/dc3671/dotfiles"
