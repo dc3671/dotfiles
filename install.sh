99e720101b3352d2d57824986c2f1254bc818216
dc3671 <chenzhh3671@gmail.com>
2025-07-22 01:11:56 -0700

    feat(zsh): add new syntax highlighting plugin

--- install.sh
+++ install.sh
@@ -85,6 +85,7 @@ msg         "[zsh] Config zsh..."
 msg         "[zsh] Install oh-my-zsh..."
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 mv ~/.zshrc ~/.zshrc.bkp >/dev/null 2>&1
 ln -sf $PWD/.zshrc ~/.zshrc
